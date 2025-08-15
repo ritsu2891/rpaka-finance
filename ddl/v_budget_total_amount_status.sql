4c922cfa19386cbe5a8b5a4b4cd5f4533469299f
Ritsuki Kokubo <ritsuki.kokubo@rpaka-farm.jp>
2025-08-15 19:25:55 +0900

    v_budget_total_amount_statusのDDLを追加

---
+++ ddl/v_budget_total_amount_status.sql
@@ -0,0 +1,17 @@
+DROP VIEW IF EXISTS v_budget_total_amount_status;
+CREATE VIEW v_budget_total_amount_status AS
+SELECT
+    SUM(set_amount) AS set_amount,
+    SUM(present_amount) AS present_amount,
+    SUM(planned_amount) AS planned_amount,
+    SUM(present_planned_amount) AS present_planned_amount,
+    SUM(present_amount) / SUM(set_amount) AS ratio_amount,
+    SUM(present_planned_amount) / SUM(set_amount) AS ratio_planned_amount,
+    SUM(GREATEST(set_amount, present_planned_amount)) AS projected_amount,
+    SUM(present_amount_credit) AS present_amount_credit,
+    SUM(planned_amount_credit) AS planned_amount_credit,
+    SUM(present_planned_amount_credit) AS present_planned_amount_credit,
+    SUM(present_amount_credit) / SUM(set_amount_credit) AS ratio_amount_credit,
+    SUM(present_planned_amount_credit) / SUM(set_amount_credit) AS ratio_planned_amount_credit,
+    SUM(GREATEST(set_amount_credit, present_planned_amount_credit)) AS projected_amount_credit
+FROM v_budget_amount_status;
