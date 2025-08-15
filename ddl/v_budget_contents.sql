dd54bd5d0d61fa0d0b7607ebf4182fd0678d652f
Ritsuki Kokubo <ritsuki.kokubo@rpaka-farm.jp>
2025-08-15 16:40:37 +0900

    v_budget_contentsのDDLを追加

---
+++ ddl/v_budget_contents.sql
@@ -0,0 +1,16 @@
+SELECT
+    m_b.title,
+    CASE
+        WHEN m_b.type = 'mf_category_l' THEN 'MF大項目'
+        WHEN m_b.type = 'mf_category_m' THEN 'MF中項目'
+    END AS type,
+    CASE
+        WHEN m_b.type = 'mf_category_l' THEN m_c_l.title
+        WHEN m_b.type = 'mf_category_m' THEN ARRAY_TO_STRING(ARRAY_AGG(m_c_m.title), ', ')
+    END AS category_title
+FROM m_budget m_b
+LEFT JOIN m_mf_category_l m_c_l ON m_b.m_mf_category_l_id = m_c_l.id
+LEFT JOIN _nc_m2m_m_mf_category_m_m_budget m2m_m_c_m ON m_b.id = m2m_m_c_m.m_budget_id
+LEFT JOIN m_mf_category_m m_c_m ON m2m_m_c_m.m_mf_category_m_id = m_c_m.id
+GROUP BY m_b.title, m_b.type, m_c_l.title, m_b.nc_order
+ORDER BY m_b.nc_order;
